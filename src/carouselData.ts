af1f18c0067c73f6641fc093a7d35b41d0420233
RenanLorijola <renanzago@gmail.com>
2023-07-15 18:44:50 -0300

    feat: team carousel

---
+++ src/carouselData.ts
@@ -0,0 +1,101 @@
+export const carouselTeamButtons = [
+  {
+    label: 'especialista anterior',
+    icon: 'M15 18L9 12L15 6',
+  },
+  {
+    label: 'próximo especilista',
+    icon: 'M9 18L15 12L9 6',
+  },
+];
+
+type CarouselTeamItems = {
+  title: string;
+  description: string;
+  img: string;
+}[];
+
+export const carouselTeamItems: CarouselTeamItems = [
+  {
+    title: 'Rafael Duarte, MD',
+    description: 'Professor de pneumologia do curso RD Step 1.',
+    img: 'rafael-duarte',
+  },
+  {
+    title: 'Eduado Antônio, MD',
+    description:
+      'Coordenador geral da RD medicine e Professor de Cardiologia do RD Step 1',
+    img: 'eduardo-antonio',
+  },
+  {
+    title: 'Mariana Montebeller, MD',
+    description:
+      'Coordenadora e professora de patologia geral e bioquímica do curso RD Step 1.',
+    img: 'mariana-montebeller',
+  },
+  {
+    title: 'Juliana Guerra, MD',
+    description: 'Professora de Endocrino do curso RD Step 1.',
+    img: 'juliana-guerra',
+  },
+  {
+    title: 'Priscila Almeida, MD',
+    description: 'Professora de Microbiologia no RD Step 1',
+    img: 'priscila-almeida',
+  },
+  {
+    title: 'Lucas Mitre, MS',
+    description: 'Professor de Musculoskeletal & Dermatology no RD Step 1.',
+    img: 'lucas-mitre',
+  },
+  {
+    title: 'Lucas Chierici, MD',
+    description:
+      'Professor de Pharmacology e Reproductive System no RD Step 1.',
+    img: 'lucas-chierici',
+  },
+  {
+    title: 'Ramon M M Vilela MD, MBA',
+    description: 'Professor de GI no curso RD Step 1',
+    img: 'ramon-vilela',
+  },
+  {
+    title: 'Sávio Carvalho, MD',
+    description: 'Professor de Nefrologia do curso RD Step 1',
+    img: 'savio-carvalho',
+  },
+  {
+    title: 'Ana David, MD',
+    description: 'Professora de Psiquiatria do curso RD step 1',
+    img: 'ana-david',
+  },
+  {
+    title: 'Marcos Firme, MD',
+    description: 'Professor de Neurology do curso RD Step 1',
+    img: 'marcos-firme',
+  },
+  {
+    title: 'Nome 1',
+    description:
+      'Clinical Research Coordinator no Miami VA Geriatric Department (GRECC). Médico ECFMG certified. Clínico pelo IAMSPE  e Cardiologista pelo Instituto Dante Pazzanase.',
+    img: 'nome-1',
+  },
+  {
+    title: 'Leonardo Saraiva, MD',
+    description:
+      'Professor de Immunology, Biostatistics, Public Health Sciences no RD Step 1.',
+    img: 'leonardo-saraiva',
+  },
+  {
+    title: 'Nome 2',
+    description:
+      'Médica formada pela Faculdade Ciencias Medicas de Minas Gerais (FCMMG) em 2021. Atualmente trabalha como Post-doctoral Research Fellow no Massachusetts General Hospital (MGH), afilido a Harvard. ECFMG certified, aplicando para o MATCH 2024 em Pediatria.',
+    img: 'nome-2',
+  },
+  {
+    title: 'Nome 3',
+    description:
+      'Médico formado pela Universidade Estadual de Pernambuco (UPE – 2021). Research Fellow em Neurologia no Mass General Hospital junto a Harvard Medical School. É aplicante para a residência de Neurologia para o MATCH 2024.',
+    img: 'nome-3',
+  },
+];
---
+++ src/utils/carousel.ts
@@ -0,0 +1,49 @@
+type initCarouselProps = {
+  carouselSelector: string;
+  buttonSelectors: string[];
+  carouselItems: object[];
+  itemWidth: number;
+  itemSpacing: number;
+};
+
+export default function initCarousel({
+  carouselSelector,
+  buttonSelectors,
+  carouselItems,
+  itemWidth,
+  itemSpacing,
+}: initCarouselProps) {
+  buttonSelectors.forEach((selector, index) => {
+    document
+      .querySelector(selector)!
+      .addEventListener('click', index ? nextSlide : prevSlide);
+  });
+  window.addEventListener('resize', () => {
+    const previousInitialActive = initialActive;
+    initialActive = Math.floor(
+      document.querySelector(carouselSelector)!.clientWidth /
+        (itemWidth + itemSpacing),
+    );
+    active = active + initialActive - previousInitialActive;
+  });
+  let initialActive = Math.floor(
+    document.querySelector(carouselSelector)!.clientWidth /
+      (itemWidth + itemSpacing),
+  );
+  let active = initialActive;
+
+  function scrollToActive() {
+    document.querySelector(carouselSelector)!.scrollTo({
+      left: (active - initialActive) * (itemWidth + itemSpacing),
+      behavior: 'smooth',
+    });
+  }
+  function nextSlide() {
+    active = active === carouselItems.length ? initialActive : active + 1;
+    scrollToActive();
+  }
+  function prevSlide() {
+    active = active === initialActive ? carouselItems.length : active - 1;
+    scrollToActive();
+  }
+}
