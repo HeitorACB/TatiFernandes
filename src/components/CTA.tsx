27def7fed8ba28bd9644da4fe40984bb8095010f
RenanLorijola <renanzago@gmail.com>
2023-07-19 14:58:57 -0300

    feat: CTA anchor

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,15 +1,33 @@
 import 'solid-js';
+import type { JSX } from 'solid-js';
 import type { ComponentWithChildren } from 'ts-utils';
 
-const CTA: ComponentWithChildren<{ className?: string }> = (props) => {
+type CTAProps = JSX.HTMLAttributes<HTMLButtonElement> &
+  JSX.HTMLAttributes<HTMLAnchorElement> & { className?: string };
+
+const CTA: ComponentWithChildren<CTAProps> = (props) => {
   return (
-    <button
-      class={`min-w-[217px] rounded-[70px] bg-[#203DE2] py-3 text-center text-sm font-semibold leading-6  ${
-        props.className || ''
-      }`}
-    >
-      {props.children}
-    </button>
+    <>
+      {'href' in props ? (
+        <a
+          {...(props as JSX.HTMLAttributes<HTMLAnchorElement>)}
+          class={`min-w-[217px] rounded-[70px] bg-[#203DE2] py-3 text-center text-sm font-semibold leading-6  ${
+            props.className || ''
+          }`}
+        >
+          {props.children}
+        </a>
+      ) : (
+        <button
+          {...props}
+          class={`min-w-[217px] rounded-[70px] bg-[#203DE2] py-3 text-center text-sm font-semibold leading-6  ${
+            props.className || ''
+          }`}
+        >
+          {props.children}
+        </button>
+      )}
+    </>
   );
 };
 
