dec716ff749402c06c190d5f1ae235f50bb987e7
RenanLorijola <renanzago@gmail.com>
2023-07-20 14:09:31 -0300

    feat: remove here word from form

--- src/utils/changeFormLanguage.ts
+++ src/utils/changeFormLanguage.ts
@@ -1,12 +1,12 @@
 const changeFormLanguage = (lang: 'pt-BR' | 'en') => {
   const placeholderName =
-    lang === 'pt-BR' ? 'Escreva seu nome aqui' : 'Write your name here';
+    lang === 'pt-BR' ? 'Escreva seu nome' : 'Write your name';
   const placeholderEmail =
-    lang === 'pt-BR' ? 'Escreva seu e-mail aqui' : 'Write your email here';
+    lang === 'pt-BR' ? 'Escreva seu e-mail' : 'Write your email';
   const placeholderPhone =
     lang === 'pt-BR'
-      ? 'Escreva seu número de celular aqui'
-      : 'Write your phone number here';
+      ? 'Escreva seu número de celular'
+      : 'Write your phone number';
 
   document
     .querySelector('form input[name="name"]')
